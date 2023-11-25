a1df9b0b5b0473f84d52e65bdfae74ee29c77cf1
Lam Tan Nghi <93366437+nghilt7@users.noreply.github.com>
2023-11-25 14:02:14 +0700

    TD0003

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CardComponent } from './components/card/component/card.component';
+
+@NgModule({
+  declarations: [CardComponent],
+  imports: [CommonModule],
+  exports: [CardComponent],
+})
+export class SharedModule {}
